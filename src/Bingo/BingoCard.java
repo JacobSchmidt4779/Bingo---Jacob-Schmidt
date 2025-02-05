d7ca4c62206355b7c7c241fa0db15c486e577cdd
Jacob Schmidt <schmidtj4779@my.uwstout.edu>
2025-02-04 20:18:33 -0600

    initial commit, created BingoCard Class

---
+++ src/Bingo/BingoCard.java
@@ -0,0 +1,11 @@
+package Bingo;
+
+public class BingoCard {
+    private String name;
+    private int[][] cardNumbers;
+
+    public BingoCard() {
+        this.name = new String();
+        this.cardNumbers = new int[5][5];
+    }
+}
