070ead910de142bf0784b71f31240a13697df549
Jacob Schmidt <schmidtj4779@my.uwstout.edu>
2025-02-08 20:11:59 -0600

    added default constructor for BingoGame, added binary search method and addToCalledNums method to add numbers to calledNums in natural order

--- src/Bingo/BingoGame.java
+++ src/Bingo/BingoGame.java
@@ -1,5 +1,47 @@
 package Bingo;
 
-public class BingoGame {
+import java.util.ArrayList;
 
+public class BingoGame {
+    private ArrayList<Integer> calledNums;
+
+    public BingoGame() {
+        this.calledNums = new ArrayList<Integer>();
+    }
+
+    public int getIndexOfOrderedInsert(int t, int low, int high) {
+		int half = (Integer) ((high-low)/2) + low;
+        if(t == calledNums.get(low) || t == calledNums.get(high)) {
+            return -1;
+        }
+		if(half == low) {
+            if (t > calledNums.get(high)) {
+                return high + 1;
+            }
+			return low;
+		}
+		if(t < calledNums.get(half)) {
+			return getIndexOfOrderedInsert(t, low, half - 1);
+		}
+		if(t > calledNums.get(half)) {
+			return getIndexOfOrderedInsert(t, half + 1, high);
+		}
+		return -1;
+	}
+
+    public boolean addToCalledNums(int num) {
+        if(num < 1 || num > 75) return false;
+        if(calledNums.size() == 0) {
+            calledNums.add(num);
+            return true;
+        }
+        int insertIndex = getIndexOfOrderedInsert(num, 0, calledNums.size() - 1);
+        if(insertIndex == -1) return false;
+        if(insertIndex >= calledNums.size()) {
+            calledNums.addLast(num);
+            return true;
+        }
+        calledNums.add(insertIndex, num);
+        return true;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
 import java.util.ArrayList;
 
 import Bingo.BingoCard;
+import Bingo.BingoGame;
 
 public class Main {
     public static void main(String[] args) {
-        BingoCard.HAS_FREE_SPACE = true;
-        ArrayList<BingoCard> cards = BingoCard.createCardsFromFile("BingoCards.txt");
-        cards.get(0).markSpot(4,1);;
-        for (BingoCard target : cards) {
-            System.out.println(target + "\n");
-        }
+        // BingoCard.HAS_FREE_SPACE = true;
+        // ArrayList<BingoCard> cards = BingoCard.createCardsFromFile("BingoCards.txt");
+        // cards.get(0).markSpot(4,1);;
+        // for (BingoCard target : cards) {
+        //     System.out.println(target + "\n");
+        // }
+        BingoGame game = new BingoGame();
+        System.out.println("1,2,3,4,5,8");
+        System.out.println(game.getIndexOfOrderedInsert(0, 0, 5));
+        System.out.println(game.getIndexOfOrderedInsert(6, 0, 5));
+        System.out.println(game.getIndexOfOrderedInsert(9, 0, 5));
+        System.out.println(game.getIndexOfOrderedInsert(3, 0, 5));
     }
 }
