836f2bf238fb048a99ae2b9f67c3245463ec64c0
Jacob Schmidt <schmidtj4779@my.uwstout.edu>
2025-02-18 14:03:04 -0600

    added method StartGame to request user for a bingo game mode and branch to the matching game mode method, added methods for playing a random game and for playing a manual game

--- src/Bingo/BingoGame.java
+++ src/Bingo/BingoGame.java
@@ -2,15 +2,52 @@ package Bingo;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BingoGame {
-    private ArrayList<Integer> calledNums;
+    private ArrayList<Integer> calledNums;  // ArrayList to hold all previously called numbers
+    private ArrayList<BingoCard> cards;     // ArrayList to hold 1 to 4
 
     /* 
      * Default Constructor
      */
     public BingoGame() {
         this.calledNums = new ArrayList<Integer>();
+    }
+
+    public void StartGame() {
+        System.out.println("What game mode would you like to play: random or manual?");
+        Scanner scan = new Scanner(System.in);
+        String ans = "";
+
+        while (ans != "random" || ans != "manual") {
+            ans = scan.nextLine().trim();
+        }
+
+        scan.close();
+
+        if (ans.equals("random")) 
+            playRandomGame();
+        else 
+            playManualGame();
+    }
+
+    public void playRandomGame() {
+        System.out.println("How many bingo cards would you like to play with?\nEnter a number from 1 to a max of 4 bingo cards");
+        String ans = "";
+        Scanner scan = new Scanner(System.in);
+        Pattern numPattern = Pattern.compile("^([1-4])$");
+        Matcher match;
+        do {
+            ans = scan.nextLine().trim();
+            match = numPattern.matcher(ans);
+        } while (match.matches());
+    }
+
+    public void playManualGame() {
+        //TODO
     }
     
     /* 
@@ -24,7 +61,7 @@ public class BingoGame {
      */
     private int getIndexOfOrderedInsert(int t, int low, int high) {
 		int half = (Integer) ((high-low)/2) + low;
-		if(high == low) {
+		if (high == low) {
             if (t > calledNums.get(high)) {
                 return high + 1;
             }
@@ -33,10 +70,10 @@ public class BingoGame {
             }
 			return low;
 		}
-		if(t < calledNums.get(half)) {
+		if (t < calledNums.get(half)) {
 			return getIndexOfOrderedInsert(t, low, half);
 		}
-		if(t > calledNums.get(half)) {
+		if (t > calledNums.get(half)) {
 			return getIndexOfOrderedInsert(t, half + 1, high);
 		}
 		return -1;
@@ -48,14 +85,14 @@ public class BingoGame {
      * @return true if num was inserted, otherwise return false 
      */
     public boolean addToCalledNums(int num) {
-        if(num < 1 || num > 75) return false;
-        if(calledNums.size() == 0) {
+        if (num < 1 || num > 75) return false;
+        if (calledNums.size() == 0) {
             calledNums.add(num);
             return true;
         }
         int insertIndex = getIndexOfOrderedInsert(num, 0, calledNums.size() - 1);
-        if(insertIndex == -1) return false;
-        if(insertIndex >= calledNums.size()) {
+        if (insertIndex == -1) return false;
+        if (insertIndex >= calledNums.size()) {
             calledNums.addLast(num);
             return true;
         }
@@ -68,7 +105,7 @@ public class BingoGame {
      * @return the random number that was generated
      */
     public int generateNumber() {
-        if(calledNums.size() == 75) return -1;
+        if (calledNums.size() == 75) return -1;
         int res = (int) (Math.random() * (75 - calledNums.size())) + 1;
         boolean cont = true;
         while (cont) {
@@ -76,6 +113,14 @@ public class BingoGame {
             res = ((res + 1) % 75) + 1;
         }
         return res;
+    }
+
+    public void generateBingoCards(int amount) {
+        ArrayList<BingoCard> temp = BingoCard.createCardsFromFile("BingoCards.txt");
+        for (int i = 0; i < amount; i++) {
+            int randIndex = (int) (Math.random() * temp.size());
+            cards.add(temp.remove(randIndex));
+        }
     }
 
     /* 
