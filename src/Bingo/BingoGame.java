627f1163fa3b173cf5b3c1da34e8d70f4e37a8ca
Jacob Schmidt <schmidtj4779@my.uwstout.edu>
2025-02-18 13:00:46 -0600

    added comments to BingoGame

--- src/Bingo/BingoGame.java
+++ src/Bingo/BingoGame.java
@@ -6,10 +6,22 @@ import java.util.Iterator;
 public class BingoGame {
     private ArrayList<Integer> calledNums;
 
+    /* 
+     * Default Constructor
+     */
     public BingoGame() {
         this.calledNums = new ArrayList<Integer>();
     }
     
+    /* 
+     * Method to search calledNums for a specified integer
+     * @param int t - integer to search for
+     * @param int low - lower bound of array to search
+     * @param int high - upper bound of array to search
+     * @return int index where the integer should be inserted into calledNums maintain natural order
+     *         otherwise return -1 if the integer is less than 1 or greater than 75 or 
+     *         the integer already exists in calledNums
+     */
     private int getIndexOfOrderedInsert(int t, int low, int high) {
 		int half = (Integer) ((high-low)/2) + low;
 		if(high == low) {
@@ -30,6 +42,11 @@ public class BingoGame {
 		return -1;
 	}
 
+    /* 
+     * Adds the specified num to calledNums while maintaining natural order
+     * @param int num - integer to insert
+     * @return true if num was inserted, otherwise return false 
+     */
     public boolean addToCalledNums(int num) {
         if(num < 1 || num > 75) return false;
         if(calledNums.size() == 0) {
@@ -46,6 +63,10 @@ public class BingoGame {
         return true;
     }
 
+    /* 
+     * Generates a random number between 1 and 75 inclusive that has not been called during this game
+     * @return the random number that was generated
+     */
     public int generateNumber() {
         if(calledNums.size() == 75) return -1;
         int res = (int) (Math.random() * (75 - calledNums.size())) + 1;
@@ -57,6 +78,10 @@ public class BingoGame {
         return res;
     }
 
+    /* 
+     * Generates a readable list of all numbers previously called
+     * @return String containing the list
+     */
     @Override
     public String toString() {
         String res = "";
