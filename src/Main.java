9a1a976331d7e01babbdd6152033c2062a288924
Jacob Schmidt <schmidtj4779@my.uwstout.edu>
2025-02-19 14:56:54 -0600

    Cleared some lines in main that were used for testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 import Bingo.BingoCard;
 import Bingo.BingoGame;
 
@@ -8,23 +6,5 @@ public class Main {
         BingoCard.HAS_FREE_SPACE = false;
         BingoGame game = new BingoGame();
         game.StartGame();
-        
-        // for (BingoCard target : cards) {
-        //     System.out.println(target + "\n");
-        // }
-        // BingoGame game = new BingoGame();
-        // for (int i = 0; i < 75; i ++) {
-        //     System.out.println("Rand num: " + game.generateNumber() + " called: " + game);
-        // }
-        // System.out.println(BingoCard.charToCol('B'));
-        // System.out.println(BingoCard.charToCol('I'));
-        // System.out.println(BingoCard.charToCol('N'));
-        // System.out.println(BingoCard.charToCol('G'));
-        // System.out.println(BingoCard.charToCol('O'));
-        // System.out.println(BingoCard.charToCol('b'));
-        // System.out.println(BingoCard.charToCol('i'));
-        // System.out.println(BingoCard.charToCol('n'));
-        // System.out.println(BingoCard.charToCol('g'));
-        // System.out.println(BingoCard.charToCol('o'));
     }
 }
