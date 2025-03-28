086c0725f664c5d6cdd1182bb72ba150db78b75d
Jacob Schmidt <schmidtj4779@my.uwstout.edu>
2025-03-28 18:58:52 -0500

    Fixed formatting

--- src/Bingo/BingoRowPattern.java
+++ src/Bingo/BingoRowPattern.java
@@ -4,12 +4,16 @@ public class BingoRowPattern implements BingoPattern {
 
     public boolean checkBingo(BingoCard card) {
         BingoCardSpace[][] spaces = card.getSpaces();
+
         for (int row = 0; row < spaces.length; row++) {
-                boolean res = true;
-                for (int col = 0; col < spaces[0].length; col++) {
-                    res = res && spaces[row][col].isMarked();
-                }
-                if (res) return res;
+            boolean res = true;
+
+            for (int col = 0; col < spaces[0].length; col++) {
+                res = res && spaces[row][col].isMarked();
+            }
+
+            if (res)
+                return res;
         }
         return false;
     }
