46ccc2455da5b11a83b1e2c1538b234e07c5f52c
Jacob Schmidt <schmidtj4779@my.uwstout.edu>
2025-04-01 22:41:20 -0500

    Implemented bingo manager

--- src/BingoPatterns/BingoManager.java
+++ src/BingoPatterns/BingoManager.java
@@ -1,5 +1,27 @@
 package BingoPatterns;
 
+import java.util.ArrayList;
+
+import Bingo.BingoCard;
+
 public class BingoManager {
-    
+    ArrayList<BingoPattern> patterns;
+
+    public BingoManager() {
+        patterns = new ArrayList<BingoPattern>();
+    }
+
+    public void addPattern(BingoPattern pattern) {
+        patterns.add(pattern);
+    }
+
+    public int numberOfBingos(BingoCard card) {
+        int bingoCount = 0;
+
+        for (int i = 0; i < patterns.size(); i++) {
+            bingoCount += patterns.get(i).numberOfBingos(card);
+        }
+
+        return bingoCount;
+    }
 }
